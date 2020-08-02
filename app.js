a9b08f3931886d83e1b5a9ebb0780a85baa932bf
sidkapoor97 <siddharthakapoor2@gmail.com>
2020-08-02 14:20:46 +0530

    Adding deleting item

--- app.js
+++ app.js
@@ -109,7 +109,12 @@ var UIController = (function() {
         inputValue : '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel : '.budget__value',
+        incomeLabel : '.budget__income--value',
+        expensesLabel : '.budget__expenses--value',
+        percentageLabel : '.budget__expenses--percentage',
+        container : '.container'
     }
 
     return {
@@ -163,6 +168,21 @@ var UIController = (function() {
             fieldsArray[0].focus();
         },
 
+        displayBudget: function(obj){
+
+            document.querySelector(DOMStrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMStrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMStrings.expensesLabel).textContent = obj.totalExp;
+
+            if(obj.percentage > 0) {
+                document.querySelector(DOMStrings.percentageLabel).textContent = obj.percentage+' %';
+            } else {
+                document.querySelector(DOMStrings.percentageLabel).textContent = '---';
+            } 
+
+
+        },
+
         getDOMStrings: function(){
             return DOMStrings;
         }
@@ -190,6 +210,8 @@ var controller = (function(budgetCtrl, UICtrl){
                 ctrlAddItem();
             }
         });
+
+        document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
     };
     
     
@@ -204,7 +226,7 @@ var controller = (function(budgetCtrl, UICtrl){
         budget = budgetCtrl.getBudget();
 
         // 3. Display the budget
-        console.log(budget);
+        UICtrl.displayBudget(budget);
 
     };
 
@@ -229,12 +251,23 @@ var controller = (function(budgetCtrl, UICtrl){
                 updateBudget();
             }
 
-    }
+    };
+
+    var ctrlDeleteItem = function(event) {
+        console.log(event.target);
+
+    };
 
 
     return {
         init: function(){
             console.log('Application has started.');
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc : 0,
+                totalExp : 0,
+                percentage: 0
+            });
             setupEventListeners();
         }
     }
